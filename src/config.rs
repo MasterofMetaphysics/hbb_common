61096aa69e9b488c63fec91e2178c56bb09682c9
qings225 <qings225@163.com>
2025-08-01 10:07:53 +0800

    修改ID/中继服务器

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["sh.deepli.cn"];
+pub const RS_PUB_KEY: &str = "H2jPSf1VA1Gbp1Ath3hclvmb2JmTL3zx9t1+95zHnFU=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
